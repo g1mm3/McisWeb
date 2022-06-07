ab08900d6daef89ea339908c5897821b0ac30b44
DESKTOP-I6CJQME\1 <checkirdy@yandex.ru>
2022-06-07 14:42:51 +0400

    Add BindRequired attribute to some models

--- McisWeb/Models/Pack.cs
+++ McisWeb/Models/Pack.cs
@@ -1,8 +1,11 @@
-﻿namespace McisWeb.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace McisWeb.Models
 {
     public class Pack
     {
         public int Id { get; set; }
+        [BindRequired]
         public string Title { get; set; }
         public bool IsFinished { get; set; }
 
